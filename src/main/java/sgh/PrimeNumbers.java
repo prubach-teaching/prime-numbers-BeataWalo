479337f16bc1c7bf743e44e91864995cacf12705
BeataWalo <bw81958@sgh.waw.pl>
2020-03-29 16:46:30 +0200

    PrimeNumbers- done

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -3,12 +3,31 @@ package sgh;
 public class PrimeNumbers {
 
     public static void primes(int n) {
-        // Your code here
-        System.out.println("2, 3, 5, 7");
+        int count = 0;
+        int x = 0;
+        int i;
+        int j = 1;
+        while (x < n) {
+            for (i = 1; i <= j; i++) {
+                if (j % i == 0) {
+                    count = count + 1;
+                }
+            }
+            if (count == 2) {
+                x = x + 1;
+                if (x < 4) {
+                    System.out.print(j + ", ");
+                } else {
+                    System.out.print(j);
+                }
+            }
+            j ++;
+            count = 0;
+        }
     }
 
-
     public static void main(String[] args) {
+        System.out.print("n = 4: ");
         primes(4);
     }
 }
